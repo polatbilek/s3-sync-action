5c024a0ef1142e4eddc3b34fad948a462433cc89
polatbilek <polatbilekozan@hotmail.com>
2022-04-16 22:00:14 +0300

    deneme

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,8 +35,35 @@ EOF
 
 sh -c "zip -rq ./${PROJECT_NAME}.zip ./${PROJECT_NAME}/"
 sh -c "aws s3 cp ./${PROJECT_NAME}.zip s3://${AWS_S3_BUCKET}/${DEST_DIR} --profile s3-sync-action --no-progress"
-aws s3api list-objects-v2 --bucket ${AWS_REQUIREMENTS_BUCKET} --query "contains(Contents[].Key, 'requirements.txt')"
-sh -c "aws s3 cp ./${PROJECT_NAME}.zip s3://${AWS_S3_BUCKET}/${DEST_DIR} --profile s3-sync-action --no-progress"
+
+reqcontains="$(aws s3api list-objects-v2 --bucket ${AWS_REQUIREMENTS_BUCKET} --query "contains(Contents[].Key, 'requirements.txt')")"
+
+echo $reqcontains
+
+# Deploy Requirements package if needed
+if [[ $reqcontains =~ "true" ]]; then
+  echo "inside"
+
+
+  sh -c "mkdir .tmp"
+  sh -c "aws s3 cp s3://${AWS_REQUIREMENTS_BUCKET}/requirements.txt ./.tmp --profile s3-sync-action --no-progress"
+
+  if [[ -f ./.tmp/requirements.txt ]]; then
+    if [[ $(diff requirements.txt ./.tmp/requirements.txt) ]]; then
+      sh -c "pip install -r requirements.txt --target ./python"
+      sh -c "rm -rf ./python/*.dist-info"
+      sh -c "zip -rq ./python.zip ./python/"
+      sh -c "rm -rf ./python"
+      sh -c "aws s3 cp ./python.zip $(PROJECT_NAME)-requirements"
+      sh -c "rm -rf ./python.zip"
+      sh -c "aws s3 cp ./requirements.txt s3://${AWS_REQUIREMENTS_BUCKET}/requirements.txt --profile s3-sync-action --no-progress"
+      echo "Deployed requirements"
+    fi
+  fi
+  else
+    echo "outside"
+fi
+sh -c "rm -r ./.tmp"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
