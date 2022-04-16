2a4d6543c01a2031859b1cc137e534070f81d320
Ozan Polatbilek <polatbilekozan@hotmail.com>
2022-04-16 21:28:22 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,6 @@ if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
 fi
 
-# Default to us-east-1 if AWS_REGION not set.
-if [ -z "$PROJECT_NAME" ]; then
-  echo "PROJECT_NAME is not set. Quitting."
-  exit 1
-fi
-
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
 if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
@@ -45,38 +39,10 @@ EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-#sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-#              --profile s3-sync-action \
-#              --no-progress \
-#              ${ENDPOINT_APPEND} $*"
-
-# Deploy lambda codes
-zip -rq ./${PROJECT_NAME}.zip ./${PROJECT_NAME}/
-aws s3 cp s3://${AWS_REQUIREMENTS_BUCKET}/python.zip . --profile s3-sync-action --no-progress
-aws s3 cp ./${PROJECT_NAME}.zip s3://${AWS_S3_BUCKET}/${DEST_DIR} --profile s3-sync-action --no-progress
-
-# Deploy Requirements package if needed
-if [[ $(aws s3api list-objects-v2 --bucket ${AWS_REQUIREMENTS_BUCKET} --query "contains(Contents[].Key, 'requirements.txt')") ]]; then
-  echo "inside"
-  echo $(aws s3api list-objects-v2 --bucket ${AWS_REQUIREMENTS_BUCKET} --query "contains(Contents[].Key, 'requirements.txt')")
-  
-  sh -c "mkdir .tmp"
-  sh -c "aws s3 cp s3://${AWS_REQUIREMENTS_BUCKET}/requirements.txt ./.tmp --profile s3-sync-action --no-progress"
-
-  if [[ -f ./.tmp/requirements.txt ]]; then
-    if [[ diff requirements.txt ./.tmp/requirements.txt ]]; then
-      sh -c "pip install -r requirements.txt --target ./python"
-      sh -c "rm -rf ./python/*.dist-info"
-      sh -c "zip -rq ./python.zip ./python/"
-      sh -c "rm -rf ./python"
-      sh -c "aws s3 cp ./python.zip $(PROJECT_NAME)-requirements"
-      sh -c "rm -rf ./python.zip"
-      sh -c "aws s3 cp ./requirements.txt s3://${AWS_REQUIREMENTS_BUCKET}/requirements.txt --profile s3-sync-action --no-progress"
-      echo "Deployed requirements"
-    fi
-  fi
-fi
-sh -c "rm -r ./.tmp"
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile s3-sync-action \
+              --no-progress \
+              ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
